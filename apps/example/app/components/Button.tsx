6273b2c471087a50f188a85295efd04659c8ed68
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-09-20 15:10:02 +0200

    Remove tertiary

--- apps/example/app/components/Button.tsx
+++ apps/example/app/components/Button.tsx
@@ -5,14 +5,13 @@ import { forwardRef } from "react";
 type ButtonProps = {
   loading?: boolean;
 } & (
-  | { primary: boolean; secondary?: boolean; tertiary?: boolean }
-  | { primary?: boolean; secondary: boolean; tertiary?: boolean }
-  | { primary?: boolean; secondary?: boolean; tertiary: boolean }
+  | { primary: boolean; secondary?: boolean }
+  | { primary?: boolean; secondary: boolean }
 ) &
   JSX.IntrinsicElements["button"];
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ primary, secondary, tertiary, loading, children, ...props }, ref) => {
+  ({ primary, secondary, loading, children, ...props }, ref) => {
     return (
       <button
         ref={ref}
@@ -23,8 +22,6 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             "bg-indigo-600 text-white hover:bg-indigo-500": primary,
             "bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50":
               secondary,
-            "bg-white hover:bg-gray-50 shadow-none text-indigo-600 ring-1 ring-inset ring-gray-100":
-              tertiary,
           },
           "flex items-center justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-100 disabled:text-black disabled:hover:bg-gray-100",
 
