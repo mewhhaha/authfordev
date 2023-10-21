68d806d7341da4b4f7381ab78cf48b1fea79d77b
Jacob Torrång <jacob@juni.co>
2023-10-21 19:55:03 +0200

    Just transition transform and shadow

--- apps/example/app/routes/home.tsx
+++ apps/example/app/routes/home.tsx
@@ -225,7 +225,7 @@ const DetailsPasskey = ({
     <details
       {...props}
       className={cn(
-        "group text-ellipsis bg-white shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] ring-1 ring-gray-200 transition-all open:translate-x-[1px] open:translate-y-[1px] open:shadow-none",
+        "group text-ellipsis bg-white shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] ring-1 ring-gray-200 transition-[transform,shadow] open:translate-x-[1px] open:translate-y-[1px] open:shadow-none",
         { "ring-amber-600": current },
         props.className
       )}
