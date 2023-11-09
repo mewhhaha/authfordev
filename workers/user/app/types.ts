53ea2c2eb6e91836ef47a87f2cab010c451d6efc
Jacob Torrång <jacob@juni.co>
2023-11-09 22:51:34 +0100

    Fuck up types

--- workers/user/app/types.ts
+++ workers/user/app/types.ts
@@ -12,18 +12,12 @@ type TaggedDurableObjectNamespace<T extends string> = TaggedType<
 
 declare global {
   type Env = {
-    API_URL_MAILCHANNELS: string;
-    SECRET_FOR_SERVER: TaggedType<string, "secret_for_server">;
-    SECRET_FOR_CLIENT: TaggedType<string, "secret_for_client">;
-    SECRET_FOR_PASSKEY: TaggedType<string, "secret_for_passkey">;
-    SECRET_FOR_SEND_CODE: TaggedType<string, "secret_for_send_code">;
-    SECRET_FOR_ALIAS: TaggedType<string, "secret_for_alias">;
-    D1: D1Database;
+    SECRET_KEY: TaggedDurableObjectNamespace<"secret_sign">;
     DO_USER: TaggedDurableObjectNamespace<"do_user">;
     DO_CHALLENGE: TaggedDurableObjectNamespace<"do_challenge">;
     DO_PASSKEY: TaggedDurableObjectNamespace<"do_passkey">;
-    KV_ALIAS: TaggedType<KVNamespace, "kv_alias">;
-    DKIM_PRIVATE_KEY: TaggedType<string, "dkim_private_key">;
+    DO_ALIAS: TaggedDurableObjectNamespace<"do_alias">;
+    KV_CACHE: TaggedType<KVNamespace, "kv_cache">;
   };
 }
 
