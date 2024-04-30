3d10396d12c9c8c05bccda0124fd5885cae2b972
Jacob Torrång <jacob.torrang@pagero.com>
2024-04-30 15:20:42 +0200

    update deps

--- workers/user/vitest.config.ts
+++ workers/user/vitest.config.ts
@@ -1,14 +1,8 @@
 // @ts-check
 // vitest.config.js
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
-import { esbuildDecorators } from "esbuild-decorators";
 
 export default defineWorkersConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildDecorators()],
-    },
-  },
   test: {
     poolOptions: {
       workers: {
